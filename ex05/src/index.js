2eb9023baf2553c8124c54848f446d02c535c452
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-04 12:02:49 +0200

    Vjezba 5

---
+++ ex05/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+  console.log("addThree: " + sum);
+}
+function addFive() {
+  sum = sum + 5;
+  console.log("addFive: " + sum);
+}
+addThree();
+addFive();
+// Only change code above this line
+module.exports = {
+  addThree,
+  addFive,
+};
