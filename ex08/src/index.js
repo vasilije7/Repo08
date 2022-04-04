14690e84585522006b65b5de1a51bae980477a7a
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-04 12:36:54 +0200

    Vjezba 8

---
+++ ex08/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+var total = 0;
+function funcWithArg(digit) {
+  total = (digit + 9) / 5;
+  return total;
+}
+funcWithArg(6);
+// Only change code above this line
+console.log(total);
+module.exports = funcWithArg;
