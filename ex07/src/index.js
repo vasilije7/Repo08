1a90aa5eab62f8a90d5529f41d639d79c47669ad
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-04 12:21:19 +0200

    Vjezba 7

---
+++ ex07/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+var dress = "little dress";
+function myDress() {
+  dress = "ball gown";
+  return dress;
+}
+console.log(myDress());
+// Only change code above this line
+module.exports = myDress;
