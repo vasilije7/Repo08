daaff1f4ec750b8c2e91ffcc36e3cdfe9c29e681
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-04 14:24:24 +0200

    Dodata vjezba 13 i prepravljena ex05

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -3,14 +3,15 @@ var sum = 0;
 
 function addThree() {
   sum = sum + 3;
-  console.log("addThree: " + sum);
+  console.log("sum from addThree: " + sum);
 }
+
 function addFive() {
   sum = sum + 5;
-  console.log("addFive: " + sum);
+  console.log("sum from addFive: " + sum);
 }
-addThree();
-addFive();
+console.log(addThree());
+console.log(addFive());
 // Only change code above this line
 module.exports = {
   addThree,
