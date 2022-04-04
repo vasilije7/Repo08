703e550230f6a66d9a12f26f86d1fcfab804aacc
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-04 09:41:03 +0200

    Vjezba 1

---
+++ ex01/src/index.js
@@ -0,0 +1,8 @@
+// Write code below this line
+function reusuableFunction() {
+  console.log("Hi Padawan!");
+}
+reusuableFunction();
+// Write code above this line
+
+module.exports = reusuableFunction;
